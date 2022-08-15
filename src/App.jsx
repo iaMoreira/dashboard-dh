ac57584f454073b338f7dd6bd67d60a7a155bc48
Ian Moreira <ianmoreira80@gmail.com>
2022-08-15 20:24:17 -0300

    start up

---
+++ src/App.jsx
@@ -0,0 +1,265 @@
+import './App.css';
+import logo from "./assets/images/logo-DH.png";
+import walke from "./assets/images/jordan-walke.png"
+import mandalorian from "./assets/images/mandalorian.jpg";
+
+function App() {
+  return (
+    <div id="wrapper">
+
+		<ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+			<a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+				<div className="sidebar-brand-icon">
+					<img className="w-100" src={logo} alt="Digital House"/>
+				</div>
+			</a>
+
+			<hr className="sidebar-divider my-0"/>
+
+			<li className="nav-item active">
+				<a className="nav-link" href="/">
+					<i className="fas fa-fw fa-tachometer-alt"></i>
+					<span>Dashboard - DH movies</span></a>
+			</li>
+
+			<hr className="sidebar-divider"/>
+
+			<div className="sidebar-heading">Actions</div>
+
+			<li className="nav-item">
+				<a className="nav-link collapsed" href="/">
+					<i className="fas fa-fw fa-folder"></i>
+					<span>Pages</span>
+				</a>
+			</li>
+
+			<li className="nav-item">
+				<a className="nav-link" href="/">
+					<i className="fas fa-fw fa-chart-area"></i>
+					<span>Charts</span></a>
+			</li>
+
+			<li className="nav-item">
+				<a className="nav-link" href="/">
+					<i className="fas fa-fw fa-table"></i>
+					<span>Tables</span></a>
+			</li>
+
+			<hr className="sidebar-divider d-none d-md-block"/>
+		</ul>
+
+		<div id="content-wrapper" className="d-flex flex-column">
+
+			<div id="content">
+
+				<nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+					<button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+						<i className="fa fa-bars"></i>
+					</button>
+
+					<ul className="navbar-nav ml-auto">
+
+						<li className="nav-item dropdown no-arrow mx-1">
+							<a className="nav-link dropdown-toggle" href="/" id="alertsDropdown">
+								<i className="fas fa-bell fa-fw"></i>
+								<span className="badge badge-danger badge-counter">3+</span>
+							</a>
+						</li>
+
+						<li className="nav-item dropdown no-arrow mx-1">
+							<a className="nav-link dropdown-toggle" href="/" id="messagesDropdown">
+								<i className="fas fa-envelope fa-fw"></i>
+								<span className="badge badge-danger badge-counter">7</span>
+							</a>
+						</li>
+
+						<div className="topbar-divider d-none d-sm-block"></div>
+
+						<li className="nav-item dropdown no-arrow">
+							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
+								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Jordan Walke</span>
+								<img className="img-profile rounded-circle" src={walke} alt="Jordan Walke - Creador de React" width="60"/>
+							</a>
+						</li>
+
+					</ul>
+
+				</nav>
+
+				<div className="container-fluid">
+					<div className="d-sm-flex align-items-center justify-content-between mb-4">
+						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+					</div>
+				
+					<div className="row">
+
+						<div className="col-md-4 mb-4">
+							<div className="card border-left-primary shadow h-100 py-2">
+								<div className="card-body">
+									<div className="row no-gutters align-items-center">
+										<div className="col mr-2">
+											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
+										</div>
+										<div className="col-auto">
+											<i className="fas fa-film fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4 mb-4">
+							<div className="card border-left-success shadow h-100 py-2">
+								<div className="card-body">
+									<div className="row no-gutters align-items-center">
+										<div className="col mr-2">
+											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
+										</div>
+										<div className="col-auto">
+											<i className="fas fa-award fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4 mb-4">
+							<div className="card border-left-warning shadow h-100 py-2">
+								<div className="card-body">
+									<div className="row no-gutters align-items-center">
+										<div className="col mr-2">
+											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
+											</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
+										</div>
+										<div className="col-auto">
+											<i className="fas fa-user fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+	
+					<div className="row">
+						<div className="col-lg-6 mb-4">
+							<div className="card shadow mb-4">
+								<div className="card-header py-3">
+									<h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
+								</div>
+								<div className="card-body">
+									<div className="text-center">
+										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "40rem"}} src={mandalorian} alt=" Star Wars - Mandalorian "/>
+									</div>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
+									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-lg-6 mb-4">						
+							<div className="card shadow mb-4">
+								<div className="card-header py-3">
+									<h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+								</div>
+								<div className="card-body">
+									<div className="row">
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Acción
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Animación
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Aventura
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Ciencia Ficción
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Comedia
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Documental
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Drama
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Fantasia
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Infantiles
+												</div>
+											</div>
+										</div>
+										<div className="col-lg-6 mb-4">
+											<div className="card bg-dark text-white shadow">
+												<div className="card-body">
+													Musical
+												</div>
+											</div>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<footer className="sticky-footer bg-white">
+				<div className="container my-auto">
+					<div className="copyright text-center my-auto">
+						<span>Copyright &copy; Dashboard 2021</span>
+					</div>
+				</div>
+			</footer>
+
+		</div>
+
+	</div>
+  );
+}
+
+export default App;
---
+++ src/components/Element/index.jsx
@@ -0,0 +1,7 @@
+import './Element.css'
+
+const Element = (props) => {
+    return <h1>Hello {props.nome}</h1>
+}
+
+export default Element
